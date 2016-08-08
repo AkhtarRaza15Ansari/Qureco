9b8bb028f9a488312b11e72a77d4eed4eca18f24
Akhtar <akhtarrazait15@gmail.com>
2016-08-09 02:05:41 +0530

    Updated Commit

--- app/src/main/java/com/sriyaan/reciever/SmsBroadReciever.java
+++ app/src/main/java/com/sriyaan/reciever/SmsBroadReciever.java
@@ -1,8 +1,16 @@
 package com.sriyaan.reciever;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
 /**
  * Created by Akhtar on 09-08-2016.
  */
-public class SmsBroadReciever  {
+public class SmsBroadReciever extends BroadcastReceiver {
 
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
 }
