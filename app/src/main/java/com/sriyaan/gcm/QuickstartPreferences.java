467e43d4d3ce7b272db57d8d70c77dd13dd96205
Ansari Akhtar Raza <akhtarrazait15@gmail.com>
2016-09-25 12:10:12 +0530

    Commit from MacBook Air

---
+++ app/src/main/java/com/sriyaan/gcm/QuickstartPreferences.java
@@ -0,0 +1,11 @@
+package com.sriyaan.gcm;
+
+/**
+ * Created by ansariakhtar on 25/09/16.
+ */
+public class QuickstartPreferences {
+
+    public static final String SENT_TOKEN_TO_SERVER = "sentTokenToServer";
+    public static final String REGISTRATION_COMPLETE = "registrationComplete";
+
+}
