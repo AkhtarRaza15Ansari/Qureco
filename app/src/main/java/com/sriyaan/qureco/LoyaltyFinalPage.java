888ee1ea4bee7d371f5ca7809ffd71b99f79a5d0
Ansari Akhtar Raza <akhtarrazait15@gmail.com>
2016-10-20 09:06:47 +0530

    Commit from MacBook Air

--- app/src/main/java/com/sriyaan/qureco/LoyaltyFinalPage.java
+++ app/src/main/java/com/sriyaan/qureco/LoyaltyFinalPage.java
@@ -11,14 +11,12 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.sriyaan.adapter.RecyclerAdapterRatings;
 import com.sriyaan.util.url_dump;
 
 import org.json.JSONArray;
